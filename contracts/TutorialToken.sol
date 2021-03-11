6bf22bed77cb20c70044196bbb3ac8653c8cf6b8
fintanduffy <fintand@gmail.com>
2021-03-11 09:50:15 +0000

    Latest Updates

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -4,12 +4,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract TutorialToken is ERC20 {
 
-  // string public override name = "TutorialToken";
-  // string public override symbol = "TT";
-  // uint8 public decimals = 2;
-  // uint public INITIAL_SUPPLY = 12000;
-
-  // constructor(uint256 initialSupply) public ERC20("Tutorial", "TT") {
   constructor() public ERC20("Tutorial", "TT") {
      _mint(msg.sender, 12000); 
   } 
